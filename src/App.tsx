df2081060827b7e8d7d960bbac02b939ab1af8a4
Rishik Reddy Cheruku <crishikreddy@gmail.com>
2025-07-15 10:05:18 +0530

    GitHub Pages Update

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio/"> {/* Add basename here */}
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Add all custom routes above the catch-all "*" route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
